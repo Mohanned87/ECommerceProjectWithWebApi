baf9370582be805012b7bbe6c4fb6fb1ad14208d
muhammed hasimioğlu <muhammed.hasimioglu@outlook.com>
2023-01-02 15:43:00 +0300

    WebAPIWithCoreMvc.Controllers added  UsersController and List index

---
+++ WebAPIWithCoreMvc/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIWithCoreMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Defines
+        private readonly HttpClient _httpClient;
+
+
+        private string url = "https://localhost:7176/api/";
+        #endregion Defines
+
+        #region Constructor
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        #endregion Constructor
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url+"Users/GetList");
+            return View(users);
+        }
+    }
+}
